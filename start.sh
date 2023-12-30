93c924ab282bbe0346a86fdcfed1c4e0bf0a0aab
SRBBOT <152118854+SRBBOT@users.noreply.github.com>
2023-12-30 17:24:11 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/SRBBOT/DQ-the-file-donor.git /DQ-the-file-donor
+  git clone https://github.com/SRBBOT/DQTheFileDonor.git /DQ-the-file-donor
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQ-the-file-donor
+  git clone $UPSTREAM_REPO /DQTheFileDonor
 fi
-cd /DQTheFileDonorBot
+cd /DQTheFileDonor
 pip3 install -U -r requirements.txt
-echo "Starting DQ-The-File-Donor...."
+echo "Starting DQTheFileDonor...."
 python3 bot.py

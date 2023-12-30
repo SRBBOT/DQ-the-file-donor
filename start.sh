f88ee5ff32499ce3b19e94ed64091b348a66a946
SRBBOT <152118854+SRBBOT@users.noreply.github.com>
2023-12-30 17:38:47 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/SRBBOT/DQTheFileDonor.git /DQ-the-file-donor
+  git clone https://github.com/SRBBOT/DQTheFileDonor.git /DQTheFileDonor
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /DQTheFileDonor
